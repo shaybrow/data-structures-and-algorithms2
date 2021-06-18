327465684c3b117ca83c39c98784d363233c92ef
Seamus Brown <shay_brown_13@hotmail.com>
2021-06-18 14:56:11 -0700

    find string tests

--- challenges/lib/src/main/java/challenges/utilities/FindString.java
+++ challenges/lib/src/main/java/challenges/utilities/FindString.java
@@ -9,6 +9,7 @@ public class FindString {
     Write a function named findAnything that takes in an array of strings, along with a target string. Return an array containing only those strings from the original array that contain the target string.
     ------------------------------------------------------------------------------------------------ */
     public static List<String> findString (String [] inputArray, String target){
+        if (inputArray == null || target == null) return null;
         List<String> output = new ArrayList<>();
         for (int i = 0; i < inputArray.length; i++) {
             if (inputArray[i].contains(target)) output.add(inputArray[i]);
--- challenges/lib/src/test/java/challenges/UtilTest.java
+++ challenges/lib/src/test/java/challenges/UtilTest.java
@@ -1,7 +1,11 @@
 package challenges;
 
+import challenges.utilities.FindString;
 import challenges.utilities.FirstLetter;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -20,5 +24,18 @@ public class UtilTest {
         assertNull(FirstLetter.firstLetter(none));
     }
 
+    @Test
+    public void testFindString (){
 
+        String test = "xd";
+
+        String [] input = {"xddd", "lmaoxd", "gottem", "ex  D", "z"};
+        List<String> trueOut = FindString.findString(input, test);
+        List<String> expect = new ArrayList<>();
+        expect.add("xddd");
+        expect.add("lmaoxd");
+        assertEquals(trueOut, expect);
+
+       
+    }
 }
