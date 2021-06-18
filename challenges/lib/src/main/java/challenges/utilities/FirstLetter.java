66aa84ee94535f9ba872faf02dfdea5afd6941b9
Seamus Brown <shay_brown_13@hotmail.com>
2021-06-18 14:28:56 -0700

    first letter in string solution

---
+++ challenges/lib/src/main/java/challenges/utilities/FirstLetter.java
@@ -0,0 +1,20 @@
+package challenges.utilities;
+
+public class FirstLetter {
+    /* ------------------------------------------------------------------------------------------------
+CHALLENGE 2
+Write a function named firstLetters that takes in an array of strings and returns an array containing only the first letter of each string.
+For example, ['this is great :)', 'wow', 'whyyyyyy :(', ':)))))'] returns ['t', 'w', 'w', ':']
+------------------------------------------------------------------------------------------------ */
+    public String [] firstLetter (String input){
+        if (input == null) return null;
+        String [] brokenUp = input.split("//s+");
+        String [] output = new String[brokenUp.length];
+        for (int i = 0; i < brokenUp.length; i++) {
+            char x = brokenUp[i].charAt(0);
+            output[i] = Character.toString(x);
+        }
+        return output;
+
+    }
+}
